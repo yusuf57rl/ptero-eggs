dd8cb0a3155e1143325d4e161c36404fe43baa7a
Yusuf Senel <94597933+yusuf57rl@users.noreply.github.com>
2024-04-24 22:45:45 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 rm -rf /home/container/tmp/*
 
 echo "⟳ Starting PHP-FPM..."
-/usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+/usr/bin/php/sbin/php-fpm --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "⟳ Starting Nginx..."
 echo "✓ Successfully started"
